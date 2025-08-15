3c33befd100ee39e1f6f6b791ee6fc101adbc8eb
Neffer2 <n3f73r@gmail.com>
2025-08-15 18:10:11 -0500

    first commit

---
+++ resources/views/index.blade.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-LN+7fdVzj6u52u30Kp6M/trliBMCMKTyK833zpbD+pXdCLuTusPj697FH4R/5mcr" crossorigin="anonymous">
+    <title>Polit&eacute;cnico Gran Colombiano</title>
+</head>
+<body>
+
+    <label for="email">Email:</label>
+    <input type="text" id="email" name="email">
+    <label for="firstname">Nombre:</label>
+    <input type="text" id="firstname" name="firstname">
+
+    <label for="lastname">Apellido:</label>
+    <input type="text" id="lastname" name="lastname">
+
+    <label for="es_bachiller_">Bachiller:</label>
+    <input type="text" id="es_bachiller_" name="es_bachiller_">
+
+    <label for="ilu_habeasdata">Tratamiento de datos:</label>
+    <input type="text" id="ilu_habeasdata" name="ilu_habeasdata">
+
+    <label for="tipo_de_documento">Tipo de documento:</label>
+    <input type="text" id="tipo_de_documento" name="tipo_de_documento">
+
+    <label for="ilu_numerodocumento">Número de documento:</label>
+    <input type="text" id="ilu_numerodocumento" name="ilu_numerodocumento">
+
+    <label for="preferred_contact_method">Contacto preferido:</label>
+    <input type="text" id="preferred_contact_method" name="preferred_contact_method">
+
+    <label for="ilu_cityofresidencecolombia">Ciudad:</label>
+    <input type="text" id="ilu_cityofresidencecolombia" name="ilu_cityofresidencecolombia">
+
+    <label for="aceptacion_de_terminos_y_condiciones">Aceptación de términos y condiciones:</label>
+    <input type="text" id="aceptacion_de_terminos_y_condiciones" name="aceptacion_de_terminos_y_condiciones">
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/js/bootstrap.bundle.min.js" integrity="sha384-ndDqU0Gzau9qJ1lfW4pNLlhNTkCfHzAVBReH9diLvGRem5+R9g2FzA8ZGN954O5Q" crossorigin="anonymous"></script>
+</body>
+</html>
