7317050f7024ad436344f3ddd1a180891f61439e
Neffer2 <n3f73r@gmail.com>
2025-08-18 16:17:51 -0500

    1. Formulario Lads funcional

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -12,74 +12,101 @@
     <div class="container">
         <form method="POST" action="{{ route('store') }}"> 
             @csrf
-            <div class="row gy-2 form-container">
-                <div class="col-md-12">
-                    <h1>Formulario de recolección</h1>
-                </div>
+            <div class="row px-2 gy-2 form-container">
                 <div class="col-md-6">
                     <div class="form-group">
                         <label for="firstname">Nombre:</label>
-                        <input type="text" id="firstname" name="firstname" class="form-control">
+                        <input type="text" id="firstname" name="firstname" class="form-control" required>
+                        @error('firstname')
+                            <div class="text-danger">{{ $message }}</div>
+                        @enderror
                     </div>
                 </div>
                 <div class="col-md-6">
                     <div class="form-group">
                         <label for="lastname">Apellido:</label>
-                        <input type="text" id="lastname" name="lastname" class="form-control">
+                        <input type="text" id="lastname" name="lastname" class="form-control" required>
+                        @error('lastname')
+                            <div class="text-danger">{{ $message }}</div>
+                        @enderror
                     </div>
                 </div>
                 <div class="col-md-6">
                     <div class="form-group">
                         <label for="email">Email:</label>
-                        <input type="email" id="email" name="email" class="form-control">
+                        <input type="email" id="email" name="email" class="form-control" required>
+                        @error('email')
+                            <div class="text-danger">{{ $message }}</div>
+                        @enderror
                     </div>
                 </div>
                 <div class="col-md-6">
                     <div class="form-group">
                         <label for="mobilephone">Número:</label>
-                        <input type="text" id="mobilephone" name="mobilephone" class="form-control">
+                        <input type="text" id="mobilephone" name="mobilephone" class="form-control" required>
+                        @error('mobilephone')
+                            <div class="text-danger">{{ $message }}</div>
+                        @enderror
                     </div>
                 </div>
                 @livewire('leads.ciudades-component')
                 <div class="col-md-6">
                     <label for="es_bachiller_">Bachiller:</label>
-                    <select name="es_bachiller_" id="es_bachiller_" class="form-control">
+                    <select name="es_bachiller_" id="es_bachiller_" class="form-control" required>
                         <option value="si">Sí</option>
                         <option value="no">No</option>
                     </select>
+                    @error('es_bachiller_')
+                        <div class="text-danger">{{ $message }}</div>
+                    @enderror
                 </div>
                 <div class="col-md-6">
                     <label for="tipo_de_documento">Tipo de documento:</label>
-                    <select name="tipo_de_documento" id="tipo_de_documento" class="form-control">
+                    <select name="tipo_de_documento" id="tipo_de_documento" class="form-control" required>
                         <option value="">Seleccionar</option>
                         @foreach ($tipos_documento as $tipo)
                             <option value="{{ $tipo['code'] }}">{{ $tipo['label'] }}</option>
                         @endforeach
                     </select>
+                    @error('tipo_de_documento')
+                        <div class="text-danger">{{ $message }}</div>
+                    @enderror
                 </div>
                 <div class="col-md-6">
                     <label for="ilu_numerodocumento">Número de documento:</label>
-                    <input type="text" id="ilu_numerodocumento" name="ilu_numerodocumento" class="form-control">
+                    <input type="text" id="ilu_numerodocumento" name="ilu_numerodocumento" class="form-control" required>
+                    @error('ilu_numerodocumento')
+                        <div class="text-danger">{{ $message }}</div>
+                    @enderror
                 </div>
                 <div class="col-md-6">
                     <label for="preferred_contact_method">Contacto preferido:</label>
-                    <select name="preferred_contact_method" id="preferred_contact_method" class="form-control">
+                    <select name="preferred_contact_method" id="preferred_contact_method" class="form-control" required>
                         <option value="">Seleccionar</option>
                         @foreach ($contacto_preferido as $contacto)
                             <option value="{{ $contacto['code'] }}">{{ $contacto['label'] }}</option>
                         @endforeach
                     </select>
-                </div>
-                <div class="col-md-6">
-                    <label for="ilu_habeasdata">Tratamiento de datos:</label>
-                    <input type="checkbox" id="ilu_habeasdata" name="ilu_habeasdata" class="form-check-input">
-                </div>
-                <div class="col-md-6">
-                    <label for="aceptacion_de_terminos_y_condiciones">Aceptación de términos y condiciones:</label>
-                    <input type="checkbox" id="aceptacion_de_terminos_y_condiciones" name="aceptacion_de_terminos_y_condiciones" class="form-check-input">
+                    @error('preferred_contact_method')
+                        <div class="text-danger">{{ $message }}</div>
+                    @enderror
                 </div>
                 <div class="col-md-12">
-                    <button class="btn btn-primary" type="submit">Enviar</button>
+                    <label for="ilu_habeasdata">Tratamiento de datos:</label>
+                    <input type="checkbox" id="ilu_habeasdata" name="ilu_habeasdata" class="form-check-input" required>
+                    @error('ilu_habeasdata')
+                        <div class="text-danger">{{ $message }}</div>
+                    @enderror
+                </div>
+                <div class="col-md-12">
+                    <label for="aceptacion_de_terminos_y_condiciones">Aceptación de términos y condiciones:</label>
+                    <input type="checkbox" id="aceptacion_de_terminos_y_condiciones" name="aceptacion_de_terminos_y_condiciones" class="form-check-input" required>
+                    @error('aceptacion_de_terminos_y_condiciones')
+                        <div class="text-danger">{{ $message }}</div>
+                    @enderror
+                </div>
+                <div class="col-md-12 d-flex justify-content-center">
+                    <button class="btn btn-primary mt-2" type="submit">ENVIAR</button>
                 </div>
             </div>
         </form>
